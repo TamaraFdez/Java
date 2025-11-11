2158bf4ccc6eb2d26ee03dd172c1cf37c9c7fc6f
TamaraFdez <TamaraFdezOSMI23@gmail.com>
2025-11-11 11:32:18 +0100

    añado 2 ejercicios del examen noviembre

---
+++ ExamenDiciembre.java
@@ -0,0 +1,7 @@
+public class ExamenDiciembre {
+    public static void main(String[] args) {
+        
+    }
+    
+    
+}
--- ExamenNoviembre.java
+++ ExamenNoviembre.java
@@ -1,8 +1,13 @@
+import java.util.Arrays;
+
 public class ExamenNoviembre {
     public static void main(String[] args) {
+        System.out.println(esPseudoDomino(new int[]{2,3,3,4,4,5,5}));
         System.out.println(aNum(new char[]{'0', '0','0','6', '9', '3', '5', '4', 'C'}));
         System.out.println(tieneFormatoNIF(new char[]{'2', '2','?','6', '9', '3', '5', '4', 'C'}));
         System.out.println(esNIFCorrecto(new char[]{'2', '2','5','6', '9', '3', '5', '4', 'C'}));
+        int[] espejito = imagenEspejo(new int[]{2,4,5});
+        System.out.println(Arrays.toString(espejito));
     }
     static boolean esLetraMayus(char c){
         return 'A' <= c && c <= 'Z' && c != 'Ñ';
@@ -41,4 +46,26 @@ public class ExamenNoviembre {
          char letraEsperada = letras[numero % 23];
          return letraEsperada == nif[8];
      }
+     public static boolean esPseudoDomino(int[] numeros){
+        if(numeros.length == 0 || numeros.length ==2 )return true;
+        for(int i = 1; i < numeros.length-1 ; i+=2){
+            if(numeros[i] != numeros[i+1])return false;
+        }
+        return true;
+     }
+    public static int[] imagenEspejo(int[] n){
+    if (n == null || n.length == 0) {
+        throw new IllegalArgumentException("El Array debe contener al menos un número");
+    }
+
+    int[] espejo = new int[n.length * 2];
+
+    for (int i = 0; i < n.length; i++) {
+        espejo[i] = n[i];
+        espejo[n.length + i] = n[n.length - 1 - i];
+    }
+
+    return espejo;
+}
+
 }
